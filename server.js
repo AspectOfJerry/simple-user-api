b483639c51e70dee07a2a05a6430e7c47a2d1c56
Jerry <jerryfu_mc@outlook.com>
2022-07-14 20:27:04 -0400

    First commit

--- server.js
+++ server.js
@@ -11,17 +11,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-const users = [{
-    id: 1,
-    name: 'Jane Doe',
-    age: 22,
-},
-{
-    id: 2,
-    name: "John Doe",
-    age: 31,
-}]
+const users = [];
 
+// const users = [{
+//     id: 1,
+//     name: 'Jane Doe',
+//     age: 22,
+// },
+// {
+//     id: 2,
+//     name: "John Doe",
+//     age: 31,
+// }]
 
 // Creating the server
 app.listen(PORT, () => {
@@ -33,18 +34,19 @@ app.listen(PORT, () => {
 // Routes
 // POST
 app.post("/create", (req, res) => {
+    console.log("Incoming POST request at '/create'");
     // Create a user
     // Check if request body is empty
     if(Object.keys(req.body).length === 0) {
         return res.status(400).json({
-            message: "Request body cannot be empty",
+            message: "400: Request body cannot be empty",
         });
     }
     // Use object destructuring to get name and age
     const {name, age} = req.body;
     if(!name || !age) {
         res.status(400).json({
-            message: "Ensure you sent both name and age",
+            message: "400: Ensure you sent both name and age",
         });
     }
     const newUser = {
@@ -55,17 +57,18 @@ app.post("/create", (req, res) => {
     try {
         users.push(newUser);
         res.status(201).json({
-            message: "Successfully created a new user",
+            message: "201: Successfully created a new user",
         });
     } catch(err) {
         res.status(500).json({
-            message: "Failed to create user",
+            message: "500: Failed to create user",
         });
     }
 });
 
 // GET
 app.get("/users", (req, res) => {
+    console.log("Incoming GET request at '/users'");
     // Retrieves all users
     try {
         res.status(200).json({
@@ -73,13 +76,14 @@ app.get("/users", (req, res) => {
         });
     } catch(err) {
         res.status(500).json({
-            message: "Failed to retrieve all users",
+            message: "500: Failed to retrieve all users",
         });
     }
 });
 
 // GET
 app.get("/users/:userID", (req, res) => {
+    console.log("Incoming GET request at '/users/:userID'");
     // Returns a user by ID
     const id = parseInt(req.params.userID);
     console.log(id);
@@ -87,7 +91,7 @@ app.get("/users/:userID", (req, res) => {
         let user = users.find((user) => user.id === id);
         if(!user) {
             return res.status(404).json({
-                message: "User not found",
+                message: "404: User not found",
             });
         }
         res.status(200).json({
@@ -95,71 +99,74 @@ app.get("/users/:userID", (req, res) => {
         });
     } catch(err) {
         res.status(500).json({
-            message: "Failed to retrieve user",
+            message: "500: Failed to retrieve user",
         });
     }
 });
 
 // PUT
 app.put("/users/:userID", (req, res) => {
+    console.log("Incoming PUT request at '/users/:userID'");
     // Update a user by ID
     try {
         const id = parseInt(req.params.userID);
         let user = users.find((user) => user.id === id);
         if(!user) {
             return res.status(404).json({
-                message: "User not found",
+                message: "404: User not found",
             });
         }
         const userIDX = users.indexOf(user);
         users[userIDX].name = req.body.name || users[userIDX].name;
         users[userIDX].age = req.body.age || users[userIDX].age;
         res.status(200).json({
-            message: "Successfully updated user",
+            message: "200: Successfully updated user",
             user,
         });
     } catch(err) {
         res.status(500).json({
-            message: "Failed to retrieve user",
+            message: "500: Failed to retrieve user",
         });
     }
 });
 
 // DELETE
 app.delete('/delete/:userID', (req, res) => {
+    console.log("Incoming DELETE request at '/delete/:userID'");
     // Delete a user by ID
     try {
         const id = req.params.userID;
         let userIDX = users.findIndex((user) => user.id === id);
         if(!userIDX) {
             res.status(404).json({
-                message: "User not found",
+                message: "404: User not found",
             });
         }
         users.splice(userIDX, 1);
         res.status(200).json({
-            message: "Successfully deleted user",
+            message: "200: Successfully deleted user",
             users,
         });
     } catch(err) {
         res.status(500).json({
-            message: "Failed to delete user",
+            message: "500: Failed to delete user",
         });
     }
 });
 
 // DELETE
-app.delete('/all/users', (req, res) => {
+app.delete('/deleteall', (req, res) => {
+    console.log("Incoming request at '/deleteall'");
     // Delete all users
     try {
         users.splice(0, users.length);
         res.status(200).json({
-            message: "Successfully deleted all users",
+            message: "200: Successfully deleted all users",
             users,
         });
     } catch(err) {
         res.status(500).json({
-            message: "Failed to delete users",
+            message: "500: Failed to delete users",
             x,
         });
     }
